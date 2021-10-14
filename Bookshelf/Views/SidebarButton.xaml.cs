a21647626d137235f7adc0f6e608aa82a15154f3
Илья Задорнов <il.mozma@gmail.com>
2021-10-14 12:40:32 -0700

    Task #18 Рефакторинг

---
+++ Bookshelf/Views/SidebarButton.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Bookshelf
+{
+    /// <summary>
+    /// Interaction logic for SidebarButton.xaml
+    /// </summary>
+    public partial class SidebarButton : UserControl
+    {
+        public SidebarButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
