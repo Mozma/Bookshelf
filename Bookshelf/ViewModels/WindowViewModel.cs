338ea5bae8ecf97f7828b3fc9b99c193bb26de82
Илья Задорнов <il.mozma@gmail.com>
2021-10-14 08:55:50 -0700

    Наведён порядок в проекте

--- Bookshelf/ViewModels/WindowViewModel.cs
+++ Bookshelf/ViewModels/WindowViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Bookshelf.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Bookshelf
 {
@@ -12,19 +14,18 @@ namespace Bookshelf
     /// </summary>
     public class WindowViewModel : BaseViewModel
     {
-        private Window controlledWindow;
+        private Window currentWindow;
         private int outerMarginSize = 10;
         private int windowRadius = 5;
 
+        #region Properties
         public int ResizeBorder { get; set; } = 4;
         public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); }  }
-        
-
         public int OuterMarginSize 
         { 
             get 
             { 
-                return controlledWindow.WindowState == WindowState.Maximized ? 0 : outerMarginSize;
+                return currentWindow.WindowState == WindowState.Maximized ? 0 : outerMarginSize;
             }
             set 
             {
@@ -36,7 +37,7 @@ namespace Bookshelf
         {
             get
             {
-                return controlledWindow.WindowState == WindowState.Maximized ? 0 : windowRadius;
+                return currentWindow.WindowState == WindowState.Maximized ? 0 : windowRadius;
             }
             set
             {
@@ -44,13 +45,25 @@ namespace Bookshelf
             }
         }
         public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
-
         public int TitleHeight { get; set; } = 21;
-        public GridLength TitleHeightGridLength { get { return new GridLength(WindowRadius); } }
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+
+        public int MinHeight { get; set; } = 400;
+        public int MinWidth { get; set; } = 600;
+        #endregion
+
+        #region Commands
+
+        public ICommand MinimizeCommand { get; set; }
+        public ICommand MaximizeCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+
+        #endregion
+
         public WindowViewModel(Window window)
         {
-            controlledWindow = window;
-            controlledWindow.StateChanged += (sender, e) =>
+            currentWindow = window;
+            currentWindow.StateChanged += (sender, e) =>
             {
                 OnPropertyChanged(nameof(ResizeBorderThickness));
                 OnPropertyChanged(nameof(OuterMarginSize));
@@ -60,6 +73,12 @@ namespace Bookshelf
                 OnPropertyChanged(nameof(TitleHeight));
                 OnPropertyChanged(nameof(TitleHeightGridLength));
             };
+
+            MinimizeCommand = new RelayCommand(() => currentWindow.WindowState = WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => currentWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => currentWindow.Close());
+
+            var resizer = new WindowResizer(currentWindow);
         }
     }
 }
