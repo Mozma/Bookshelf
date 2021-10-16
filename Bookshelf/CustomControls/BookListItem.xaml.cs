4f0870dc548a5c37d8002b78cebca79f9033d0bb
Илья Задорнов <il.mozma@gmail.com>
2021-10-16 02:05:55 -0700

    Task #20 Добавлен новый контролл для элемента списка

---
+++ Bookshelf/CustomControls/BookListItem.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Bookshelf.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for SideBarMenuButton.xaml
+    /// </summary>
+    public partial class BookListItem : UserControl
+    {
+        public BookListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
