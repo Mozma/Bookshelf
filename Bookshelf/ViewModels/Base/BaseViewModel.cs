165ff5fd2f8068c989fb8aded3c3695ee0327ddd
Илья Задорнов <il.mozma@gmail.com>
2021-10-31 20:09:22 +0300

    Task #37 Добвлена кнопка "назад"

--- Bookshelf/ViewModels/Base/BaseViewModel.cs
+++ Bookshelf/ViewModels/Base/BaseViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using Bookshelf.ViewModels.Base;
+using System;
 
 namespace Bookshelf
 {
     public class BaseViewModel : ObservableObject
     {
-
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
--- Bookshelf/ViewModels/Book/ShelfViewModel.cs
+++ Bookshelf/ViewModels/Book/ShelfViewModel.cs
@@ -9,6 +9,9 @@ namespace Bookshelf.ViewModels
         private NavigationStore navigationStore;
 
         public ICommand OpenShelfCommand { get; set; }
+        public ICommand AddNewBookCommand { get; set; }
+        public ICommand GoToShelvesCommand { get; set; }
+
         public string Name { get; set; }
         public List<BookListItemViewModel> Items { get; set; }
 
@@ -24,9 +27,20 @@ namespace Bookshelf.ViewModels
         {
             this.navigationStore = navigationStore;
 
+            SetupCommands();
+        }
+
+        private void SetupCommands() 
+        {
             OpenShelfCommand = new RelayCommand(o =>
             {
                 this.navigationStore.CurrentViewModel = this;
+
+            });
+
+            GoToShelvesCommand = new RelayCommand(o =>
+            {
+                this.navigationStore.CurrentViewModel = new ShelvesViewModel(navigationStore);
 
             });
         }
